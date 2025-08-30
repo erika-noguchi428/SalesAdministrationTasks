a3651823256b27924fb88ec1f054a93a6ed70108
Erika Noguchi <you@example.com>
2025-08-30 14:26:57 +0900

    初回コミット：プロジェクト構成と初期コード

---
+++ src/test/java/task/management/SalesAdministrationTasks/SalesAdministrationTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package task.management.SalesAdministrationTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesAdministrationTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
